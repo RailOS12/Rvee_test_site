30452a1c949e3930ea4f41e3f27e12c527dc1569
salim <salim@example.com>
2025-09-29 13:22:39 +0300

    feat: initial simple site scaffold

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿const button = document.getElementById("helloBtn");
+if (button) {
+  button.addEventListener("click", () => {
+    alert("Привет от GitHub Pages!");
+  });
+}
+
+const button = document.getElementById(\helloBtn\);
+button?.addEventListener(\click\, () => {
+ alert(\РџСЂРёРІРµС РѕС GitHub Pages!\);
+});
